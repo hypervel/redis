ed8b428d80955f28346fe185b31399936dca8854
Albert Chen <albert.cht@gmail.com>
2025-09-22 15:44:52 +0800

    fix: fix initialization of pool name in RedisProxy

--- src/RedisProxy.php
+++ src/RedisProxy.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace Hypervel\Redis;
 
+use Hyperf\Redis\Pool\PoolFactory;
+
 class RedisProxy extends Redis
 {
+    public function __construct(PoolFactory $factory, string $pool)
+    {
+        parent::__construct($factory);
+
+        $this->poolName = $pool;
+    }
 }
